2113e046b10811302bf3f46c60df76e920b5c37e
Brenden Matthews <brenden@diddyinc.com>
2019-05-23 19:21:45 -0400

    Export switchroom as library, add expiry binary.

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+extern crate env_logger;
+extern crate futures;
+#[macro_use]
+extern crate log;
+extern crate chrono;
+extern crate tokio;
+extern crate tower_hyper;
+#[macro_use]
+extern crate failure;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate lazy_static;
+extern crate foundationdb;
+extern crate instrumented;
+extern crate prost;
+extern crate sodiumoxide;
+extern crate switchroom_grpc;
+extern crate toml;
+extern crate url;
+extern crate yansi;
+
+pub mod config;
+pub mod metrics;
+pub mod storage;
